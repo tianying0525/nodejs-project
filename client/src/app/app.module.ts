6ca4f4b1b9383ffe70f90afd99f41b8c85540221
Connie Zhou <connie@s-26-160.flex.volo.net>
2019-07-17 00:51:18 -0500

    init strucutre

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -3,16 +3,23 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ProblemListComponent } from './components/problem-list/problem-list.component';
+
+import { DataService } from "./services/data.service";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProblemListComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule
   ],
-  providers: [],
+  providers: [{
+  provide: "data",
+  useClass: DataService
+  }],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ client/src/app/mock.problems.ts
@@ -0,0 +1,21 @@
+import { Problem } from './models/problem.model';
+
+
+export const PROBLEMS: Problem[] = [{
+  id: 1,
+  name: "Problem 1",
+  desc: "what",
+  difficulty: "hard"
+},
+{
+  id: 2,
+  name: "Problem 1",
+  desc: "what",
+  difficulty: "hard"
+},
+{
+  id: 3,
+  name: "Problem 1",
+  desc: "what",
+  difficulty: "hard"
+}];
